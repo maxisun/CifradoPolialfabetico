e0184400fc78a0c4b8097fed5dd72d3277055195
maxisun <00354615@uca.edu.sv>
2018-08-31 21:28:22 -0600

    se logro que imprimera el alfabeto normal usando conversion de un Arraylist de numeros a su valor en la tabla del ascii

--- src/Class/Texto.java
+++ src/Class/Texto.java
@@ -1,4 +1,40 @@
 package Class;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Texto {
+
+    private ArrayList<Integer> alfabeto;
+    private String input;
+
+    public Texto(ArrayList<Integer> alfabeto, String input) {
+        this.alfabeto = alfabeto;
+        this.input = input;
+    }
+
+    public Texto() {
+    }
+
+    public ArrayList<Integer> getAlfabeto() {
+        return alfabeto;
+    }
+
+    public void setAlfabeto(ArrayList<Integer> alfabeto) {
+        this.alfabeto = alfabeto;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public void print(ArrayList<Integer> a){
+        for (int i=0;i<a.size();i++) {
+            System.out.print((char) a.get(i).byteValue());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import Class.Texto;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        ArrayList<Integer> ascii = new ArrayList<>();
+
+        for(int i=65;i<91;i++){
+            ascii.add(i);
+        }
+
+        Texto texto = new Texto(ascii,"1");
+
+        texto.print(ascii);
+
     }
 }
