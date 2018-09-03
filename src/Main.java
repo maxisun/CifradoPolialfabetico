b89069cb9f78820b711616df89bdf6ba31fe0406
maxisun <00354615@uca.edu.sv>
2018-09-02 19:34:34 -0600

    solo falta validar los datos de entrada

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         List<Integer> lista = new ArrayList<>();
+        List<Integer> lista2 = new ArrayList<>();
         int[] ascii = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,209,79,80,81,82,83,84,85,86,87,88,89,90,48,49,50,51,52,53,54,55,56,57}; //37
 
         for(int i=0;i<ascii.length;i++){
             lista.add(ascii[i]);
+            lista2.add(ascii[i]);
         }
 
         System.out.println("Ingrese cifrado:");
@@ -27,7 +29,25 @@ public class Main {
 
         StringBuffer buffer = new StringBuffer();
 
-        /*
+        for(int i=0;i<a;i++){ //recorre los alfabetos
+            if(direcciones[cont_letras].equals("I")){
+                Collections.rotate(lista2,Integer.parseInt(alfabeto[cont_numeros]));
+            }else if(direcciones[cont_letras].equals("D")){
+                Collections.rotate(lista2,-(Integer.parseInt(alfabeto[cont_numeros])));
+            }
+            cont_letras++;
+            cont_numeros++;
+        }
+
+        int pene2 =0;
+        for (int j=0;j<palabra.length();j++){
+            pene2 = lista.indexOf((int)palabra.charAt(j));
+            buffer.append((char) Integer.parseInt(String.valueOf(lista2.get(pene2))));
+        }
+        System.out.println(buffer.toString());
+
+
+                /*
         buffer.append((char)(palabra.charAt(0)-Integer.parseInt(alfabeto[cont_numeros])));
         buffer.append((char)(palabra.charAt(1)-Integer.parseInt(alfabeto[cont_numeros])));
         //System.out.println(palabra.charAt(0)-1);
@@ -56,19 +76,20 @@ public class Main {
             System.out.println(buffer.toString());
         }
 */
-        int pene = 0;
 
+/*
+        int pene = 0;
         for(int i=0;i<a;i++){ //recorre los alfabetos
             for(int j=0;j<palabra.length();j++){ //recorre la palabra
                 if(direcciones[cont_letras].equals("I")){
                     Collections.rotate(lista,Integer.parseInt(alfabeto[cont_numeros]));
                     pene = lista.indexOf((int)palabra.charAt(j));
-                    buffer.append((char) Integer.parseInt(String.valueOf(lista.get(pene + Integer.parseInt(alfabeto[cont_numeros]) ))));
-
+                    System.out.println(pene);
+                    //buffer.append((char) Integer.parseInt(String.valueOf(lista.get(pene + Integer.parseInt(alfabeto[cont_numeros]) ))));
                 }else if(direcciones[cont_letras].equals("D")){
-                    Collections.rotate(lista,Integer.parseInt(alfabeto[cont_numeros]));
-                    pene = lista.indexOf((int)palabra.charAt(j));
-                    buffer.append((char) Integer.parseInt(String.valueOf(lista.get(pene - Integer.parseInt(alfabeto[cont_numeros]) ))));
+                    Collections.rotate(lista,-Integer.parseInt(alfabeto[cont_numeros]));
+                    //pene = lista.indexOf((int)palabra.charAt(j));
+                    //buffer.append((char) Integer.parseInt(String.valueOf(lista.get(pene - Integer.parseInt(alfabeto[cont_numeros])))));
                 }
             }
             cont_letras++;
@@ -77,6 +98,8 @@ public class Main {
             palabra = buffer.toString();
             buffer.setLength(0);
         }
+
+        */
         //System.out.print(buffer.toString());
 
     }
